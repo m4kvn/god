509de9e3b811bce2254a1d428cdcf1f6c3ad4c98
m4kvn <fun.masahiro@gmail.com>
2018-04-03 00:42:25 +0900

    Update at 2018-04-03 00:42:25

--- installs/gems/tw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ ! -n "$(gem list | grep -w tw)" ]; then
-    gem install tw
-fi
--- installs/install.sh
+++ installs/install.sh
@@ -14,23 +14,44 @@ install_homebrew_cask() {
     BREW_CASK_LIST=$(mktemp $DIR/tmp/brew-cask-list.XXXXXXXX)
     brew cask list > $BREW_CASK_LIST
     (cat $DIR/homebrew-cask $BREW_CASK_LIST | sort -u; cat $BREW_CASK_LIST) | \
-    sort | uniq -u | xargs brew cask install
+    sort | uniq -u | xargs -I{} brew cask install {}
     rm -f $BREW_CASK_LIST
 }
 
 install_homebrew() {
-    :
+    BREW_LIST=$(mktemp $DIR/tmp/brew-list.XXXXXXXX)
+    brew list > $BREW_LIST
+    (cat $DIR/homebrew $BREW_LIST | sort -u; cat $BREW_LIST) | \
+    sort | uniq -u | xargs -I{} brew install {}
+    rm -f $BREW_LIST
+}
+
+install_shell() {
+    find $DIR/shell -type f | xargs -I{} sh {}
+}
+
+install_lang() {
+    find $DIR/lang -type f | xargs -I{} sh {}
+}
+
+install_gem() {
+    GEM_LIST=$(mktemp $DIR/tmp/gem.XXXXXXXX)
+    gem list | cut -d " " -f 1 > $GEM_LIST
+    (cat $DIR/gem $GEM_LIST | sort -u; cat $GEM_LIST) | \
+    sort | uniq -u | xargs -I{} gem install {}
+    rm -f $GEM_LIST
+}
+
+install_etc() {
+    find $DIR/etc -type f | xargs -I{} sh {}
 }
 
 install_homebrew_tap
 install_homebrew_cask
 install_homebrew
-
-# find $DIR/homebrew-cask -type f | xargs -I{} sh {}
-# find $DIR/homebrew -type f | xargs -I{} sh {}
-# find $DIR/etc -type f | xargs -I{} sh {}
-#
-# [ -n "$(gem --version 2>/dev/null)" ] \
-#     && find $DIR/gems -type f | xargs -I{} sh {}
+install_shell
+install_lang
+install_gem
+install_etc
 
 exit 0
